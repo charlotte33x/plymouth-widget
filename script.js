638234ae00632b89e3d99bd27a445037ea12d3ca
charlotte33x <charlotte.verney@yahoo.com>
2026-09-16 16:29:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 async function loadWeather() {
 
     const response = await fetch(
-        "https://api.open-meteo.com/v1/forecast?latitude=50.3755&longitude=-4.1427&current=temperature_2m&daily=temperature_2m_max&forecast_days=3"
+        "https://api.open-meteo.com/v1/forecast?latitude=50.3755&longitude=-4.1427&current=temperature_2m&daily=temperature_2m_max,temperature_2m_min&forecast_days=3"
     );
 
     const data = await response.json();
@@ -9,10 +9,16 @@ async function loadWeather() {
     document.getElementById("temperature").innerHTML =
         `Air: ${data.current.temperature_2m}°C`;
 
+    const days = data.daily.time;
+
     document.getElementById("forecast").innerHTML =
-        `${data.daily.temperature_2m_max[0]}° |
-         ${data.daily.temperature_2m_max[1]}° |
-         ${data.daily.temperature_2m_max[2]}°`;
+        `
+        <b>${new Date(days[0]).toLocaleDateString('en-GB',{weekday:'short'})}</b> ${Math.round(data.daily.temperature_2m_max[0])}°
+        &nbsp;|&nbsp;
+        <b>${new Date(days[1]).toLocaleDateString('en-GB',{weekday:'short'})}</b> ${Math.round(data.daily.temperature_2m_max[1])}°
+        &nbsp;|&nbsp;
+        <b>${new Date(days[2]).toLocaleDateString('en-GB',{weekday:'short'})}</b> ${Math.round(data.daily.temperature_2m_max[2])}°
+        `;
 }
 
 loadWeather();
